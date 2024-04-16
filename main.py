31d54ef38cdb1a4b3b0ea00d7c18466f1d800112
M-aka-r <94246448+M-aka-r@users.noreply.github.com>
2024-04-16 17:43:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from pygame import *
 from random import randint
+from time import sleep
+
+from Shooter.shooter_game import shooter_game, restart_shooter
+from labirint.maze import labirint_game, restart_labirint
+from Korsinka.korsinka import korsinka_game
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 500, 500
 WINDOW = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -40,38 +45,53 @@ class Button(GameSprite):
 
 # ! Функции кнопок
 def func_btn1():
-    print("КНОПКА 1 НАЖАТА")
+    global game, WINDOW
+    
+    mixer.music.load("Shooter//dream.mp3")
+    mixer.music.play(-1)
+    mixer.music.set_volume(0.2)
+    
+    restart_shooter()
+    
+    game = 2 
+    WINDOW = display.set_mode((700, 500))
 
 def func_btn2():
-    print("КНОПКА 2 НАЖАТА")
+    
+    global game, WINDOW
+    
+    mixer.music.load("labirint//jugles.mp3")
+    mixer.music.play()
+    
+    restart_labirint()
+    
+    game = 3
+    WINDOW = display.set_mode((500, 500))
 
 def func_btn3():
-    print("КНОПКА 3 НАЖАТА")
-
-def func_btn4():
-    import Korsinka.korsinka
     
+    global game, WINDOW
+    
+    korsinka_game()
+    
+    game = 4
+    WINDOW = display.set_mode((700, 500))
     
 # ! Спрайты
 btn1 = Button(img="btn.png", 
-              position=(96, 100), 
+              position=(46, 200), 
               size=(104, 104), 
               func=func_btn1)
 
-btn2 = Button(img="btn2.png", 
-              position=(300, 100), 
+btn2 = Button(img="btn3.png", 
+              position=(196, 200), 
               size=(104, 104), 
               func=func_btn2)
 
-btn3 = Button(img="btn3.png", 
-              position=(300, 296), 
+btn3 = Button(img="btn4.png", 
+              position=(356, 200), 
               size=(104, 104), 
               func=func_btn3)
-
-btn4 = Button(img="btn4.png", 
-              position=(96, 296), 
-              size=(104, 104), 
-              func=func_btn4)
 
 
 
@@ -87,14 +107,16 @@ mixer.music.set_volume(0.5)
 # ! Текст
 font.init()
 stats_font = font.Font("code.ttf", 42)
+controll_font = font.Font("code.ttf", 22)
 name_text = stats_font.render("Mastergame", True, (20, 130, 200))
+controll_text = controll_font.render("A D    W A S D    A D", True, (20, 130, 120))
 # ?
 # ?
 
 game = 1
 
 btns_group = sprite.Group()
-btns_group.add(btn1, btn2, btn3, btn4)
+btns_group.add(btn1, btn2, btn3)
 
 # ?
 
@@ -107,14 +129,51 @@ while GAME_RUN:
         if ev.type == MOUSEBUTTONDOWN:
             mouse_x, mouse_y = ev.pos
             for btn in btns_group:
-                if btn.rect.collidepoint(mouse_x, mouse_y):
-                    GAME_RUN = False
+                if btn.rect.collidepoint(mouse_x, mouse_y) and game == 1:
                     btn.func()
 
     if not GAME_FINISHED:
+        
         if game == 1:
+            WINDOW.fill((0, 0, 0))
             WINDOW.blit(name_text, (70, 10))
+            WINDOW.blit(controll_text, (56, 360))
             btns_group.draw(WINDOW)
+            
+        if game == 2:
+            is_ended = shooter_game()
+            
+            if is_ended:
+                sleep(2)
+        
+                mixer.music.load("bg.mp3")
+                mixer.music.play(-1)
+                
+                game = 1
+                WINDOW = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+            
+        if game == 3:
+            is_ended = labirint_game()
+            
+            if is_ended:
+                sleep(2)
+        
+                mixer.music.load("bg.mp3")
+                mixer.music.play(-1)
+                
+                game = 1
+                WINDOW = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+            
+        if game == 4:
+            is_ended = korsinka_game()
+            
+            if is_ended:
+        
+                mixer.music.load("bg.mp3")
+                mixer.music.play(-1)
+                
+                game = 1
+                WINDOW = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     
     display.update()
     CLOCK.tick(FPS)
